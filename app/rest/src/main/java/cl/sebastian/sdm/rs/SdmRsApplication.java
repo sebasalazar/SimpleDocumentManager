f59af667c3f9590c392ccf68b9324e5ab1d2180d
Sebastián Salazar Molina <sebasalazar@gmail.com>
2018-09-07 23:43:11 -0300

    Inicio del proyecto rest

---
+++ app/rest/src/main/java/cl/sebastian/sdm/rs/SdmRsApplication.java
@@ -0,0 +1,12 @@
+package cl.sebastian.sdm.rs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdmRsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdmRsApplication.class, args);
+	}
+}
